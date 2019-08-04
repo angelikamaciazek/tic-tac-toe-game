aca8b6ac2332f4b11bfa7af0b128cc99510a42d7
Angelika <maciazekangelika@gmail.com>
2019-08-04 22:42:09 +0200

    add squares and onCLick to Board

--- src/index.js
+++ src/index.js
@@ -12,13 +12,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
 
   handleClick(i) {
     const squares = this.state.squares.slice()
@@ -33,8 +26,11 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)} />
+    return (<Square
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+    />
+    )
   }
 
   render() {
